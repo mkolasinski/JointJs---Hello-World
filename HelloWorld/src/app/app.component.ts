702324ba6c9eb492836d636137c68c8449f53fd4
mkolasinski <m.kolasinski@poczta.fm>
2017-09-28 21:08:18 +0200

    - the other version ... WIP

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,5 +1,36 @@
-import {Component} from '@angular/core';
+import * as $ from "jquery";
+// const joint = require('../../node_modules/jointjs/dist/joint');
+import {Component, NgOnInit} from '@angular/core';
 
+ngOnInit()
+{
+  let graph = new joint.dia.Graph;
+
+  let paper: dia.Paper;
+  paper = new joint.dia.Paper({
+    el: $('#myholder'),
+    width: 600,
+    height: 200,
+    model: graph,
+    gridSize: 1
+  });
+
+  let rect = new joint.shapes.basic.Rect({
+    position: {x: 100, y: 30},
+    size: {width: 100, height: 30},
+    attrs: {rect: {fill: 'blue'}, text: {text: 'my box', fill: 'white'}}
+  });
+
+  let rect2 = rect.clone();
+  rect2.translate(300);
+
+  let link = new joint.dia.Link({
+    source: {id: rect.id},
+    target: {id: rect2.id}
+  });
+
+  graph.addCells([rect, rect2, link]);
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +39,6 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
   title = 'app';
-  graph = joint.dia.Graph;
-  paper = joint.dia.Paper;
 }
+
+
