dcf7d6d9fe5c3e521441673302f42c02171ddf91
mkolasinski <m.kolasinski@poczta.fm>
2017-09-29 12:19:34 +0200

    - first active version of model...

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,44 +1,57 @@
-import * as $ from "jquery";
-// const joint = require('../../node_modules/jointjs/dist/joint');
-import {Component, NgOnInit} from '@angular/core';
+import * as $ from 'jquery';
+import * as joint from 'jointjs';
+// const joint = require('jointjs/dist/joint.js');
 
-ngOnInit()
-{
-  let graph = new joint.dia.Graph;
+// import {ngOnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-  let paper: dia.Paper;
-  paper = new joint.dia.Paper({
-    el: $('#myholder'),
-    width: 600,
-    height: 200,
-    model: graph,
-    gridSize: 1
-  });
 
-  let rect = new joint.shapes.basic.Rect({
-    position: {x: 100, y: 30},
-    size: {width: 100, height: 30},
-    attrs: {rect: {fill: 'blue'}, text: {text: 'my box', fill: 'white'}}
-  });
-
-  let rect2 = rect.clone();
-  rect2.translate(300);
-
-  let link = new joint.dia.Link({
-    source: {id: rect.id},
-    target: {id: rect2.id}
-  });
-
-  graph.addCells([rect, rect2, link]);
-};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  graph: any;
+  paper: any;
+
+  ngOnInit() {
+    this.createGraph();
+  }
+
+  private createGraph() {
+    this.graph = new joint.dia.Graph();
+    this.paper = new joint.dia.Paper({
+      el: $('#myholder'),
+      width: 600,
+      height: 200,
+      model: this.graph,
+      gridSize: 1
+    });
+
+    let rect = new joint.shapes.basic.Rect({
+      position: {x: 100, y: 30},
+      size: {width: 100, height: 30},
+      attrs: {rect: {fill: 'blue'}, text: {text: 'my box', fill: 'white'}}
+    });
+
+    let rect2 = new joint.shapes.basic.Rect({
+      position: {x: 250, y: 230},
+      size: {width: 100, height: 30},
+      attrs: {rect: {fill: 'blue'}, text: {text: 'my box', fill: 'white'}}
+    });
+
+    let link = new joint.dia.Link({
+      source: {id: rect.id},
+      target: {id: rect2.id}
+    });
+
+    this.graph.addCells([rect, rect2, link]);
+  }
 }
+
+
+
 
 
